b804fae8b771c3a28d2f288d2ce2f0ddecbfb8c8
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-13 17:55:34 +0530

    Added accordion functionality on click

--- starter/06-Components/Accordion/script.js
+++ starter/06-Components/Accordion/script.js
@@ -1,1 +1,7 @@
 `use strict`;
+const item = document.getElementsByClassName("item");
+Array.from(item).forEach((e) =>
+  e.addEventListener("click", () => {
+    e.classList.toggle("open");
+  })
+);
